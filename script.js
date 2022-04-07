39465edff40cf435395c590204ae7731d5cb1d73
Horwerty <damiowat@gmail.com>
2022-04-07 06:00:22 +0100

    second commit99

---
+++ script.js
@@ -0,0 +1,17 @@
+const submitButton = document.querySelector('.submitBtn');
+
+ submitButton.addEventListener('click', SubmitFunction);
+
+//   function SubmitFunction () {
+//         console.log(submitButton);
+//         window.location.assign('www.google.com');
+//         console.log(window.location)
+
+
+// // function SubmitFunction(){
+//      var a = document.createElement('a')
+//      a.href = "https://www.google.com"
+// };
+
+// console.log(SubmitFunction)
+//}//
